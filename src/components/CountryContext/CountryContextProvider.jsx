27e1b826895c3632c29ce4ca04fcb32bc7a127fe
Tarun Kumar Sukhu <tksukhu@gmail.com>
2020-12-02 13:43:58 +0530

    fixed context issue

--- src/components/CountryContext/CountryContextProvider.jsx
+++ src/components/CountryContext/CountryContextProvider.jsx
@@ -7,16 +7,24 @@ const CountryContextProvider = (props) => {
   const [filteredText, setFilteredText] = useState("");
   const { children } = props;
   const [filteredCountries, setFilteredCountries] = useState([]);
-  const [countries] = useState(props.countries);
+  const [countries, setCountries] = useState(props.countries || []);
 
   useEffect(() => {
-    const filteredCountries = countries.filter(
-      (country) =>
-        country.name.toLowerCase().includes(filteredText) ||
-        country.region.toLowerCase().includes(filteredText) ||
-        country.subregion.toLowerCase().includes(filteredText)
-    );
-    setFilteredCountries(filteredCountries);
+    if (props.countries) {
+      setCountries(props.countries);
+    }
+  }, [props.countries]);
+  
+  useEffect(() => {
+    if (countries) {
+      const filteredCountries = countries.filter(
+        (country) =>
+          country.name.toLowerCase().includes(filteredText) ||
+          country.region.toLowerCase().includes(filteredText) ||
+          country.subregion.toLowerCase().includes(filteredText)
+      );
+      setFilteredCountries(filteredCountries);
+    }
   }, [countries, filteredText]);
 
   return (
